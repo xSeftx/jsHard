97055dbb17202d043bda40468c28995af0daf8c4
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-10-13 22:09:24 +0400

    Усложненное задание к уроку №4

--- script/script.js
+++ script/script.js
@@ -1,40 +1,18 @@
-'use strict';
+let userName = function(name){
+    if (typeof name === 'string') {        
+        if (name.trim().length > 30) {
+            return (name.substr(0, 30) + '...');
+        
+        } else {
+            return name;
+        }
+    } else {
+        return ('Вы ввели число')
+    }
+};
+    
 
-
-let lang = prompt('Введите значение '  + '"ru" ' + 'или ' + '"en"');
-if (lang === 'ru'){
-    console.log('пн ' + 'вт ' + 'ср ' + 'чт ' + 'пт ' + 'сб ' + 'вс');
-} else if(lang === 'en'){
-    console.log('mn ' + 'ts ' + 'wd ' + 'th ' + 'fr ' + 'st ' + 'sn');
-} else {
-    console.log('Что-то пошло не так');
-}
-
-
-switch (lang){
-    case 'ru':
-        console.log('пн ' + 'вт ' + 'ср ' + 'чт ' + 'пт ' + 'сб ' + 'вс');
-        break;
-    case 'en':
-        console.log('mn ' + 'ts ' + 'wd ' + 'th ' + 'fr ' + 'st ' + 'sn');
-        break;
-    default:
-        console.log('Что-то пошло не так');
-}
-
-
-
-let objLang = {
-  ru: ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
-  en: ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
-}
-console.log(objLang[lang]);
-
-let namePerson = prompt('Введите значение: ' + '"Артем" ' + 'или ' + '"Максим"');
-let firstPerson = 'Артем';
-let secondPerson = 'Максим';
-let result = namePerson===firstPerson ? console.log('директор') : namePerson===secondPerson ? console.log('преподаватель') : console.log('студент'); 
-
+console.log(userName(' 123456789 '));
 
 
 

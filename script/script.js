b65ec87d68e6ef1c1bef6c26b841ebd52ec6061e
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-11-03 19:23:17 +0400

    Добавил задание №18 "таймер"

--- script/script.js
+++ script/script.js
@@ -1,64 +1,64 @@
 'use strict';
 
+function getTime(){
+    const data = new Date(),    
+        year = data.getFullYear(),
+        month = data.getMonth(),
+        day = data.getDate(),
+        hour = data.getHours(),
+        minutes = data.getMinutes(),
+        seconds = data.getSeconds(),
+        divSay = document.querySelector('.say'),
+        divDay = document.querySelector('.day'),
+        divTime = document.querySelector('.time'),
+        divYear = document.querySelector('.new-year'),
+        nowData = new Date().getTime(),
+        dataNewYear = new Date('1 January 2021').getTime(),
+        timeRemaining = ((dataNewYear - nowData)/1000),        
+        days = [
+        'Воскресенье',
+        'Понедельник',
+        'Вторник',
+        'Среда',
+        'Четверг',
+        'Пятница',
+        'Суббота'
+        ];
+    let mid;
 
-
-
-
-
-let clock = function() {
-    let Data = new Date(),
-    Year = Data.getFullYear(),
-
-    Month = Data.getMonth(),
-    Day = Data.getDate(),
-    Hour = Data.getHours(),
-    Minutes = Data.getMinutes(),
-    Seconds = Data.getSeconds(),
-    hour = '', 
-
-    nowMonth = ["января", "февраля", "марта", "мае", "июня", "июля", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
-
-
-
-
-
-
-let calendar = function() {
-if (Hour === 1 || Hour === 21){
-    hour = 'час';
-} else if(Hour >= 2 && Hour <= 4 || Hour >= 22 && Hour <= 24){
-    hour = 'часа';
-} else {
-    hour = 'часов';
-}return hour;
-
-};
-
-calendar();
-
-let getZero = function(nam){
-if (nam > 0 && nam < 10) { 
-    return '0' + nam;
-} else {
-    return nam;
-}
-};
+    const getZero = function(nam){
+        if (nam > 0 && nam < 10) { 
+            return '0' + nam;
+        } else {
+        return nam;
+        }
+    }
+    
+    
+    function sayHello(){    
+    if (hour > 20 || hour < 5){
+        divSay.textContent = 'Доброй ночи';
+    } else if (hour > 5 || hour < 11) {
+        divSay.textContent = 'Доброе утро';
+    } else {
+        divSay.textContent = 'Доброй день';
+    }
+    
+    divDay.textContent = `Сегодня: ${days[day-1]}`;
+    divTime.textContent = `Текущее время: ${getZero(hour)}: ${getZero(minutes)}: ${getZero(seconds)} ${hour < 12 ? mid='AM' : mid='PM'}`;
+    divYear.textContent = `До Нового года осталось ${Math.floor((timeRemaining)/60 / 60 /24)} дней`
 
     
-let timerOne = ('Сегодня ' + Day + ' ' + nowMonth[Month] + ' ' + Year + ' года ' + Hour + ' ' + hour + ' ' + Minutes + ' минут ' + Seconds + ' секунд' + '<br>' ); 
 
+    
+    }
+    
 
+    sayHello();
+}
 
-let timerTwo = ('<br>' + getZero(Hour) + ':' + getZero(Minutes) + ':' + getZero(Seconds) + ' ' + getZero(Day) + '.' + getZero(Month) + '.' + getZero(Year) + '  ');
+setInterval(getTime, 1000);
 
-
-document.getElementById("clock_one").innerHTML = timerOne;
-
-document.getElementById("clock_two").innerHTML = timerTwo;
-};
-
- 
-setInterval(clock, 1000);
 
 
 

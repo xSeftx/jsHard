c5231b5fa90f7a20c6f0799bb934dfab264aa5bd
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-11-19 21:29:02 +0400

    Добавил 14 задание усложненное

--- script/script.js
+++ script/script.js
@@ -1,17 +1,81 @@
-'use strict';
+window.addEventListener('DOMContentLoaded', function(){
 
-const btn = document.querySelector('.btn'),
-    colorText = document.querySelector('.color'),
-    body = document.querySelector('HTML');
+  'use strict';
 
-btn.addEventListener('click', () => {
-    let random = Math.random().toString(36).substring(2, 2) + Math.random().toString(16).substring(2, 16),
-        getcolor = random.slice(0,6);
-    body.style.background = `#${getcolor}`;
-    colorText.textContent = `#${getcolor}`;
-    btn.style.color = `#${getcolor}`;
-    
+
+  const DomElement = function(selector, height, width, bg, fontSize) {
+      this.selector = selector;
+      this.height = height;
+      this.width = width;
+      this.bg = bg;
+      this.fontSize = fontSize;    
+      this.newElement = function() {
+          let newElem = document.createElement('div');
+          if (this.selector[0] === '.'){
+              newElem.className = this.selector.slice(1);
+          } if (this.selector[0] === '#'){
+              newElem.setAttribute('id', this.selector.slice(1));
+          }
+          newElem.style.cssText = ` height: ${height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize}; position: absolute`;
+          // newElem.innerText = prompt('Введите текст');
+          document.body.prepend(newElem);
+
+
+      };
+      this.squareElem = function() {
+          let moveX = 0,
+              moveY = 0;
+          const square = document.querySelector('.new-class');                       
+          document.addEventListener('keydown', (event) => {  
+              console.log(event);                       
+              if(event.key === 'ArrowRight'){                                        
+                  moveX += 10;
+                  square.style.left = `${moveX}px`;
+
+              }
+              if(event.key === 'ArrowLeft'){                    
+                  moveX -= 10;
+                  square.style.left = `${moveX}px`;
+
+              }
+              if(event.key === 'ArrowUp'){                    
+                  moveY -= 10;
+                  square.style.top = `${moveY}px`;
+
+              }
+              if(event.key === 'ArrowDown'){                    
+                  moveY += 10;
+                  square.style.top = `${moveY}px`;
+
+              }
+          })
+
+      }
+
+      
+
+  }; 
+  
+  const createElem = new DomElement('.new-class', '100px', '100px', 'red', '32px');
+  
+  createElem.newElement();
+  createElem.squareElem();
+   
+
+
+
+
+
+
 });
+
+      
+
+
+  
+  
+
+
 
 
 
